c219583f5956a9b070eaa374b6369033787707b0
Stephen Yang <stefan8935@hotmail.com>
2021-04-08 09:10:14 +1000

    list split

---
+++ chat3.py
@@ -0,0 +1,12 @@
+
+lines = []
+with open('chat.txt', 'r', encoding = 'utf-8-sig') as f:
+	for line in f:
+		lines.append(line.strip())
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+	print(time)
+	print(name)
+	print(s)
